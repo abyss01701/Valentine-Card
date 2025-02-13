04136c56f95a0f9fbfc641ea29b5dbc6fe47f808
Akshat Newal <akshataniketnewal@gmail.com>
2025-02-13 14:57:14 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Valentine-Card/", // Replace with your repo name
   plugins: [react(), tailwindcss()],
 })
