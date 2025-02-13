109a0b4fbd44c0fa4b09d71be7c92df07750f866
abyss01701 <akshataniketnewal@gmail.com>
2025-02-13 13:18:57 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import emailjs from 'emailjs-com';
+
+function App() {
+
+  const [gif, setGif] = useState("/giphy.gif");
+  const [isYesClicked, setIsYesClicked] = useState(false);
+  const [scale, setScale] = useState(1);
+
+  useEffect(() =>{
+    emailjs.init("o1BNtwwJHp8NKwV85");
+  }, []);
+
+  const handleYesClick = () => {
+    setGif('/icegif-193.gif'); // Change gif on "Yes"
+    setIsYesClicked(true);   // Hide buttons and show "Yay" message
+
+    // Send email silently
+    emailjs.send(
+      'service_rdpxdek',   // Replace with your EmailJS Service ID
+      'template_rwixq1a',  // Replace with your EmailJS Template ID
+      { message: 'She said yes.' }, // Email content
+      'o1BNtwwJHp8NKwV85'    // Replace with your EmailJS Public Key
+    )
+    .then(() => {
+      console.log('Email sent successfully');
+    })
+    .catch(error => {
+      console.error('Error sending email:', error);
+    });
+  }; 
+  
+    const handleNoClick = () => {
+      setScale(prevScale => prevScale + 0.50); // Increase the scale each time "No" is clicked
+    };
+  
+  return (
+    <>
+      <div className="flex items-center justify-center mt-35 ">
+        <img src={gif} alt="Gif"/>
+      </div>
+
+      <div className="-mt-20 text-center">
+        {!isYesClicked && (
+        <h2 className="text-black text-3xl" style={{ fontFamily: "'Lucy Said OK', cursive" }}>
+          Will you be my valentine?
+        </h2>
+        )}
+
+        {/* Buttons */}
+        {!isYesClicked && (
+        <div className="flex items-center justify-center gap-5">
+          <button className="bg-green-500 text-white px-5 py-1 rounded-full font-semibold shadow-lg hover:bg-green-600 transition"
+           style={{ transform: `scale(${scale})` }} // Apply scaling dynamically
+           onClick={handleYesClick}
+          >
+            Yes
+          </button>
+          <button className="bg-red-500 text-white px-5 py-1 rounded-full font-semibold shadow-lg hover:bg-red-600 transition"
+           onClick={handleNoClick}
+          >
+            No
+          </button>
+        </div>
+         )}
+
+         {/* "Yay" message and buttons disappearance */}
+         {isYesClicked && (
+            <p className="mt-12 text-2xl font-semibold text-green-500">Yay!</p>
+          )}
+      </div>
+   
+    </>
+  );
+}
+
+export default App;
